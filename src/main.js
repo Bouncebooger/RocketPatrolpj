2bc9296220e83702fe13b291e276d33226041634
dwild <dwild@ucsc.edu>
2022-04-11 16:16:51 -0700

    Phonk

--- src/main.js
+++ src/main.js
@@ -5,5 +5,7 @@ height: 480,
 scene: [Menu,Play]
 }
 let game = new Phaser.Game(config);
+//let keyF,keyR,keyLEFT,keyRIGHT;
 let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize /3 ;
+let keyF,keyR,keyLEFT,keyRIGHT;
